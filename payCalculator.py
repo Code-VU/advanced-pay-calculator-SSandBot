ff5e830b0400893f489a3aa8a8031beb2253fa7f
Susan Sandbothe <Susan.Sandbothe@vu.com>
2022-02-17 17:24:59 -0600

    advance pay1

--- payCalculator.py
+++ payCalculator.py
@@ -1,12 +1,16 @@
-def calculatePay():
-    
-    # This first line is provided for you
-    hrs = input("Enter Hours:")
-    
-    # end assignment
-
+# This first line is provided for you
+hrs = input("Enter Hours:")
+rate = input("Enter rate:")
+Salary = float(hrs)*float(rate)
+if hrs >= 40:
+    print("Pay:",Salary)
+if hrs < 40:
+    Overtime = float(hrs-40)*1.5
+print("Pay:",Overtime)
+  
+# end assignment
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+# calculatePay 
