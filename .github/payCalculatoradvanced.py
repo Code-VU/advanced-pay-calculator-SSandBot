a3447f684c5c028da488aff13a326cd74ee460f5
Susan Sandbothe <Susan.Sandbothe@vu.com>
2022-02-17 18:49:57 -0600

    again

--- .github/payCalculatoradvanced.py
+++ .github/payCalculatoradvanced.py
@@ -1,13 +1,12 @@
-def paycalculatoradvanced()
-
+def paycalculatoradvanced():
 # This first line is provided for you
-hrs = input("Please input your hours:")
-rate = input("Enter your rate:")
-Salary = float(hrs)*float(rate)
-if float(hrs) <=40:
-    print ("Reg Time:",Salary)
-elif float(hrs) >40:
-    print ("Full Pay:",(float(hrs)-40) * (float(rate) * 1.5))
+    hrs = input("Please input your hours:")
+    rate = input("Enter your rate:")
+    Salary = float(hrs)*float(rate)
+    if float(hrs) <=40:
+        print ("Reg Time:",Salary)
+    elif float(hrs) >40:
+        print ("Full Pay:",(float(hrs)-40) * (float(rate) * 1.5))
 # end assignment
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
