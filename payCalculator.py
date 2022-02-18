6c0caea4ea7bc78c83840fb695770e4ff545bf36
Susan Sandbothe <Susan.Sandbothe@vu.com>
2022-02-17 18:29:59 -0600

    paycalc1

--- payCalculator.py
+++ payCalculator.py
@@ -1,16 +1,14 @@
 # This first line is provided for you
-hrs = input("Enter Hours:")
-rate = input("Enter rate:")
+hrs = input("Please input your hours:")
+rate = input("Enter your rate:")
 Salary = float(hrs)*float(rate)
-if hrs >= 40:
-    print("Pay:",Salary)
-if hrs < 40:
-    Overtime = float(hrs-40)*1.5
-print("Pay:",Overtime)
-  
+if float(hrs) <=40:
+    print ("Reg Time:",Salary)
+elif float(hrs) >40:
+    print ("Full Pay:",(float(hrs)-40) * (float(rate) * 1.5))
 # end assignment
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay 
+# calculatePay ()
